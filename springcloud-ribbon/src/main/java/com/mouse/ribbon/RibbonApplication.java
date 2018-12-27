fe1237c79a9884e774fc4f0700f675efa4a7e26f
shilun <sl166199@163.com>
2018-12-27 14:17:33 +0800

    ribbon

---
+++ springcloud-ribbon/src/main/java/com/mouse/ribbon/RibbonApplication.java
@@ -0,0 +1,14 @@
+package com.mouse.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApplication.class, args);
+    }
+
+}
+
