02f1d2da786d17e0ba416262026a7da43d54d78f
shilun <sl166199@163.com>
2018-12-29 17:21:05 +0800

    feign

---
+++ springcloud-feign/src/main/java/com/mouse/feign/FeignApplication.java
@@ -0,0 +1,19 @@
+package com.mouse.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableFeignClients
+public class FeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignApplication.class, args);
+    }
+
+}
+
