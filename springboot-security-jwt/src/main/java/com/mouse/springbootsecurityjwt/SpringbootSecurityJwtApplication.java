bc64dd215a59b122a62b2c2e5a9afeaf830ec6fe
shilun <sl166199@163.com>
2018-12-29 17:28:32 +0800

    历史

---
+++ springboot-security-jwt/src/main/java/com/mouse/springbootsecurityjwt/SpringbootSecurityJwtApplication.java
@@ -0,0 +1,12 @@
+package com.mouse.springbootsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurityJwtApplication.class, args);
+    }
+}
