f192e212a2c2e807c309fab5a9382b57c9ce99b3
shilun <sl166199@163.com>
2019-01-07 14:18:08 +0800

    hystrix-dashboard 监控信息

--- springcloud-feign/src/main/java/com/mouse/feign/FeignApplication.java
+++ springcloud-feign/src/main/java/com/mouse/feign/FeignApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class FeignApplication {
 
     public static void main(String[] args) {
